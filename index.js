489934d89dbb875e11501b0b3df263bfc76666e6
Keonne Gladden (KeKnows) <keonnegladden7@gmail.com>
2026-04-13 00:10:37 -0500

    Add route handlers for auth, workouts, training plans, and performance

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 console.log("INDEX FILE STARTED");
 const express = require('express');
 const { sequelize } = require('./models');
-
+const authRoutes = require('./routes/auth');
+const workoutRoutes = require('./routes/workouts');
 const app = express();
+const trainingPlanRoutes = require('./routes/trainingPlans');
+const performanceRoutes = require('./routes/performanceRecords');
+
+app.use('/records', performanceRoutes);
+app.use('/plans', trainingPlanRoutes);
+app.use('/workouts', workoutRoutes);
+
 
 // ======================
 // MIDDLEWARE
 // ======================
 app.use(express.json());
+app.use('/auth', authRoutes);
 
 // Basic logging middleware
 app.use((req, res, next) => {
